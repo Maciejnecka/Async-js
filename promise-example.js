8b3491e4327134e4837b91eae30ecc12cb4fe080
Maciej Necka <macieknecka@gmail.com>
2023-10-31 21:33:57 +0100

    Initial commit

---
+++ promise-example.js
@@ -0,0 +1,27 @@
+// callback, promises, asyc/await
+// Promises - Pending, Resolved, Rejected
+// then catch - pass another callback
+
+const heading1 = document.querySelector('.one');
+const heading2 = document.querySelector('.two');
+const heading3 = document.querySelector('.three');
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {});
+
+const promise = new Promise((resolve, reject) => {
+  let value = true;
+  if (value) {
+    resolve([1, 2, 4]);
+  } else {
+    reject(`there was a error, value is false`);
+  }
+});
+promise
+  .then((taco) => {
+    console.log(taco);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
